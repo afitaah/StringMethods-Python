3b857abe56d16f68a2b13ce267b7d034b491ce5a
caaqil <a.caaqil20@gmail.com>
2022-02-09 19:11:42 +0300

    StringMethods

---
+++ StringMethods.py
@@ -0,0 +1,27 @@
+"""
+1-capitalize() => weyneen (infarta la weyneeyo gabigeed)
+2-isalpha() => Xaruuf kaliya miyaa Returns=(True or False)
+3-isnumeric() => numbers kaliya miyaa Returns=(True or False)
+4-isupper()  =>farta ma waweynaa Returns=(True or False)
+5-.split() Waxoo u dhaxeysiina Coma(,) meel kastoo isbees ku jiro
+    Kadibne waxoo galinaa List
+6-title() =>Bilaawga erayda Woo weynenaa sidee title jumlada ugu muuqato
+7-strip() Wxoo jumlada kasaraa isbeska bilawga iyo ka dhamaadka
+8-replace() =>Xarfohoo isku badalaa Woxoona Qataa 2wo Value as argument 
+"""
+print("make this font capitalize".capitalize()) #Make this font capitalize
+
+print("This Alphabetic letters only".isalpha())
+
+print("1234".isnumeric()) #Right returns(True)
+
+print("THIS FONT IS UPPER".isupper()) #Right Returns(True)
+
+print("One Two Three".split()) #Returns ['One', 'Two', 'Three']
+
+print("welcome caaqil web page".title()) #Welcome Caaqil Web Page
+
+print(" After Space | Before space ".strip()) #"After Space | Before space"
+
+print("JS  | PHP".replace("JS","python")) #python  | PHP
+
